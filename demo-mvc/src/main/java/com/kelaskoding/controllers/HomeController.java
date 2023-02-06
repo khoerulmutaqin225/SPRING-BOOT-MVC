6fda7eb0664fd8c466cfcc5e40d0292bdc95770b
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-02-06 10:20:00 +0700

    form handling submisstion

--- demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
+++ demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kelaskoding.entity.Product;
 import com.kelaskoding.service.ProductService;
 
 @Controller
@@ -26,5 +28,18 @@ public class HomeController {
 
         return "index";
     }
-    
+
+    @GetMapping("/add")
+    public String add(Model model){
+        model.addAttribute("product", new Product(8L,"006","Product 008", 800000.0 ));
+        return "add";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product, Model model){
+        // System.out.println(product);
+        productService.addProduct(product);
+        return "redirect:/";
+    }
+
 }
