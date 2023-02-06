3cbdc3e014cfb011037eab0511dc93876c1aeba5
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-02-06 11:13:27 +0700

    Handling delete and Update Button

--- demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
+++ demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,5 +42,28 @@ public class HomeController {
         productService.addProduct(product);
         return "redirect:/";
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        // servise untuk hapus
+        productService.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model){
+        //Call ServicById
+        model.addAttribute("product",productService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product, Model model){
+        productService.updateProduct(product);
+
+        return "redirect:/";
+    }
+
+
 
 }
--- demo-mvc/src/main/java/com/kelaskoding/service/ProductService.java
+++ demo-mvc/src/main/java/com/kelaskoding/service/ProductService.java
@@ -1,21 +1,24 @@
 package com.kelaskoding.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
 import com.kelaskoding.entity.Product;
+import com.kelaskoding.utils.RandomNumber;
 
 @Service
 public class ProductService {
     private static List<Product> products = new ArrayList<Product>() {
 
         {
-            add(new Product(System.currentTimeMillis(), "001", "Product 001", 1000.0));
-            add(new Product(System.currentTimeMillis(), "002", "Product 002", 1200.0));
-            add(new Product(System.currentTimeMillis(), "003", "Product 003", 3000.0));
-            add(new Product(System.currentTimeMillis(), "004", "Product 004", 4000.0));
-            add(new Product(System.currentTimeMillis(), "005", "Product 005", 5000.0));
+            add(new Product(RandomNumber.getRandom(1000,9999), "001", "Product 001", 1000.0));
+            add(new Product(RandomNumber.getRandom(1000,9999), "002", "Product 002", 2000.0));
+            add(new Product(RandomNumber.getRandom(1000,9999), "003", "Product 003", 3000.0));
+            add(new Product(RandomNumber.getRandom(1000,9999), "004", "Product 004", 4000.0));
+            add(new Product(RandomNumber.getRandom(1000,9999), "005", "Product 005", 5000.0));
         }
     };
 
@@ -25,8 +28,24 @@ public class ProductService {
     }
 
     public void addProduct(Product product) {
-        product.setId(System.currentTimeMillis());
+        product.setId(RandomNumber.getRandom(1000,9999));
         products.add(product);
     }
+
+    public void deleteById(long id) {
+        products.removeIf(p -> p.getId() == id);
+    }
+
+    public Optional<Product> findById(long id) {
+       return products.stream().filter(p -> p.getId() == id).findFirst();
+    }
+    
+    public void updateProduct(Product product) {
+        deleteById(product.getId());
+        products.add(product);
+    }
+
+
+
 
 }
