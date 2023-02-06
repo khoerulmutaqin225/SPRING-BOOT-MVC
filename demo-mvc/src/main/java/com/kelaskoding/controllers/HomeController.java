6e88d858ef48dbd5ff02f66086b2e3c1d584f910
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-02-06 09:37:30 +0700

    mengirim  data dari controller ke view

--- demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
+++ demo-mvc/src/main/java/com/kelaskoding/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.kelaskoding.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kelaskoding.service.ProductService;
+
 @Controller
 @RequestMapping("")
 public class HomeController {
+
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping
     public String welcome(Model model){
@@ -15,6 +22,8 @@ public class HomeController {
         //Doing Something
         String message = "Welcome Spring Boot Cah Ganteng";
         model.addAttribute("msg",message);
+        model.addAttribute("products", productService.findAll());
+
         return "index";
     }
     
